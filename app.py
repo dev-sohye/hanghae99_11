e53991acbb95d15d0f59a42b9d6d8b7e7b701e1a
Isthis <rjsgmldnwn@gmail.com>
2021-11-02 21:49:17 +0900

    로그인, 회원가입, 로그인 창

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 
 app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.exhibition
+
+import jwt, datetime, hashlib
+
+SECRET_KEY = 'okay'
+
 
 #멀티 페이지 url
 @app.route('/')
@@ -19,6 +27,43 @@ def login():
 @app.route('/register')
 def register():
     return render_template("register.html")
+
+# 회원가입
+@app.route('/api/register', methods=['POST'])
+def api_register():
+    id_receive = request.form['user_id']
+    pw_receive = request.form['user_pw']
+    gender_receive = request.form['user_gender']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    db.user.insert_one({'user_id': id_receive, 'user_pw': pw_hash, 'user_gender' : gender_receive})
+
+    return jsonify({'result': 'success'})
+
+# 로그인
+@app.route('/api/login', methods=['POST'])
+def api_login():
+    id_receive = request.form['user_id']
+    pw_receive = request.form['user_pw']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    result = db.user.find_one({'user_id': id_receive, 'user_pw': pw_hash})
+    result_id = db.user.find_one({'user_id': id_receive})
+
+    if result is not None:
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    elif result_id is None:
+        return jsonify({'result': 'fail', 'msg': '아이디가 존재하지 않습니다!'})
+    else:
+        return jsonify({'result': 'fail', 'msg': '비밀번호를 확인해주세요!'})
 
 
 if __name__ == '__main__':
