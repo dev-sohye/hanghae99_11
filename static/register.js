e19b02e2bffd520dd9ed4becce298deb2c5cf093
Isthis <rjsgmldnwn@gmail.com>
2021-11-03 11:26:09 +0900

    비밀번호 일치 확인

--- static/register.js
+++ static/register.js
@@ -1,11 +1,13 @@
+//회원가입 요청
+
 function register() {
   $.ajax({
     type: "POST",
     url: "/api/register",
     data: {
-      user_id: $('#userid').val(),
-      user_pw: $('#userpw').val(),
-      user_gender: $('.usergender').val(),
+      user_id: $("#userid").val(),
+      user_pw: $("#userpw").val(),
+      user_gender: $(".usergender").val(),
     },
     success: function (response) {
       if (response["result"] == "success") {
@@ -14,10 +16,35 @@ function register() {
       } else {
         alert(response["msg"]);
       }
-    }
-  })
+    },
+  });
 }
 
-if ($('#userpw').val()==$('#pwchk').val()){
-  
+// 비밀번호 확인
+function pwchk() {
+  $(".pw").focusout(function () {
+    let pw1 = $("#userpw").val();
+    let pw2 = $("#pwchk").val();
+
+    if (pw1 == "") {
+      $("#alert-success").css("display", "none");
+      $("#alert-danger").css("display", "none");
+    } else if (pw2 == "") {
+      $("#alert-success").css("display", "none");
+      $("#alert-danger").css("display", "none");
+    } else if (pw1 != "" && pw2 != "") {
+      if (pw1 == pw2) {
+        $("#alert-success").css("display", "block");
+        $("#alert-danger").css("display", "none");
+        return true;
+      } else {
+        $("#alert-success").css("display", "none");
+        $("#alert-danger").css("display", "block");
+        return false;
+      }
+    }
+  });
 }
+
+console.log(pwchk());
+//성별 데이터 전송
