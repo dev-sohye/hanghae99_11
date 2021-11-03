c135c3a2caeeddc5c55eb2c577225ee55981ca41
Isthis <rjsgmldnwn@gmail.com>
2021-11-03 09:38:03 +0900

    유저 정보 확인 api

--- app.py
+++ app.py
@@ -13,8 +13,16 @@ SECRET_KEY = 'okay'
 
 #멀티 페이지 url
 @app.route('/')
-def main():
-    return render_template("index.html")
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user.find_one({"id": payload['id']})
+        return render_template('index.html', id=user_info["id"])
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인을 다시 해주세요."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 @app.route('/exhibition')
 def exhibition():
@@ -28,7 +36,7 @@ def login():
 def register():
     return render_template("register.html")
 
-# 회원가입
+# 회원가입 API
 @app.route('/api/register', methods=['POST'])
 def api_register():
     id_receive = request.form['user_id']
@@ -41,7 +49,7 @@ def api_register():
 
     return jsonify({'result': 'success'})
 
-# 로그인
+# 로그인 API
 @app.route('/api/login', methods=['POST'])
 def api_login():
     id_receive = request.form['user_id']
@@ -55,7 +63,7 @@ def api_login():
     if result is not None:
         payload = {
             'id': id_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60*60)
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
@@ -64,6 +72,20 @@ def api_login():
         return jsonify({'result': 'fail', 'msg': '아이디가 존재하지 않습니다!'})
     else:
         return jsonify({'result': 'fail', 'msg': '비밀번호를 확인해주세요!'})
+
+# 유저 정보 확인 API
+@app.route('/api/user', methods=['GET'])
+def api_valid():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        userinfo = db.user.find_one({'user_id': payload['id']}, {'_id': 0})
+
+        return jsonify({'result': 'success', 'id': userinfo['user_id'], 'gender': userinfo['user_gender']})
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
 
 if __name__ == '__main__':
---
+++ static/register.js
@@ -0,0 +1,19 @@
+function register() {
+  $.ajax({
+    type: "POST",
+    url: "/api/register",
+    data: {
+      user_id: $('#userid').val(),
+      user_pw: $('#userpw').val(),
+      user_gender: $('.usergender').val(),
+    },
+    success: function (response) {
+      if (response["result"] == "success") {
+        alert("회원가입이 완료되었습니다.");
+        window.location.href = "/login";
+      } else {
+        alert(response["msg"]);
+      }
+    }
+  })
+}
