33ebdf7a6f10b330366ebb914b29f07a0e8c605e
dev_yomi <81748525+dev-sohye@users.noreply.github.com>
2021-11-05 17:16:40 +0900

    fear_comment
    리뷰 작성과 보여주는 css 및 class 수정

--- static/exhibition.js
+++ static/exhibition.js
@@ -6,13 +6,13 @@ $.ajax({
  data: {},
   success: function (response) {
     if (response["result"] == "success") {
-      $(".login-btn-active").css("display", "block");
-      $(".login-btn").css("display", "none");
+      $(".customer").css("display", "block");
+      $(".visitor").css("display", "none");
       let username = response["user_id"];
       $("#customer_name").prepend(username);
     } else {
-      $(".login-btn-active").css("display", "none");
-      $(".login-btn").css("display", "block");
+      $(".customer").css("display", "none");
+      $(".visitor").css("display", "block");
     }
   },
 });
@@ -30,12 +30,10 @@ function dologin() { //리뷰창 카운터
       if (response["result"] == "success" && counterLet == 1) {
         let reviewwrite = `
          
-            <div className="review-commit">
+            <div class="review-commit">
               <ul>
-                <li className="user-info">
-                  <p>user img</p>
-                  <p>user name</p>
-                  <select id="grade" className="form-select">
+                <li class="user-info">
+                    <select id="grade" class="form-select">
                     <option selected>평점</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -44,8 +42,8 @@ function dologin() { //리뷰창 카운터
                     <option value="5">5</option>
                   </select>
                 </li>
-                <li className="table contentArea">
-                            <textarea className="form-control" id="comment"
+                <li class="table contentArea">
+                            <textarea class="form-control" id="comment"
                                       cols="30"
                                       rows="5" placeholder="내용을 입력하세요."></textarea>
                 </li>
